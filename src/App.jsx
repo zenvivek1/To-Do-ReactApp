9131aa5eae17bda6e05fe0c9194857740df12fc9
vivek <igvivekr@gmail.com>
2025-11-07 19:33:37 +0530

    7 Nov

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
   return (
     <>
     
-    <div className='w-full h-screen bg-zinc-900 text-white p-10'>
+    <div className='w-full min-h-screen bg-zinc-900 text-white p-10'>
       <nav className='w-full bg-zinc-700 h-20 flex items-center justify-center text-3xl font-bold'>TO-DO List</nav>
       <button onClick={()=>setcreateWindow(true)} className='px-5 py-3 bg-blue-300 m-8 ml-0 rounded-md font-bold text-black cursor-pointer'>Add Task</button>
       <button onClick={ClearAllTasks} className='px-5 py-3 bg-red-400 rounded-md font-bold text-black cursor-pointer'>Clear All Tasks</button>
